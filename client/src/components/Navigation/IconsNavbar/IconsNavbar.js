5b481f62d3b2402b0d57e1fdba18b1698562b1a9
Renaud <matheusvrenaud@gmail.com>
2022-01-17 02:05:04 +0100

    Grammar

--- client/src/components/Navigation/IconsNavbar/IconsNavbar.js
+++ client/src/components/Navigation/IconsNavbar/IconsNavbar.js
@@ -3,21 +3,22 @@ import React from 'react'
 import './Style.scss'
 
 function IconsNavbar() {
+    
     return (
         <div className="navbar2">
             <nav className="">
                     <a
-                        href="https://github.com/MVRenaud"
+                        href="https://github.com/MVRenaud" target={"#"}
                         className="text-decoration-none m-2 m-lg-0"
                     >
                         <i class="bi bi-github d-lg-block"></i></a>
                     <a
-                        href="https://www.linkedin.com/in/matheus-renaud-a71069110/"
+                        href="https://www.linkedin.com/in/matheus-renaud-a71069110/" target={"#"}
                         className="text-decoration-none m-2  m-lg-0"
                     >
                         <i class="bi bi-linkedin d-lg-block"></i></a>
                     <a
-                        href="https://www.facebook.com/matheus.renaud"
+                        href="https://www.facebook.com/matheus.renaud" target={"#"}
                         className="text-decoration-none m-2 m-lg-0"
                     >
                         <i class="bi bi-facebook d-lg-block"></i></a>
